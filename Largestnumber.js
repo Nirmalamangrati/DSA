403810fbdad836e2df13a5ee7b36d9458c3cd01b
Nirmalamangrati <157771329+Nirmalamangrati@users.noreply.github.com>
2026-04-20 17:40:39 +0545

    first file

---
+++ Largestnumber.js
@@ -0,0 +1,12 @@
+//Find the largest element in an array
+let arr = [10, 5, 20, 8];
+
+let max = arr[0];
+
+for (let i = 1; i < arr.length; i++) {
+  if (arr[i] > max) {
+    max = arr[i];
+  }
+}
+
+console.log("Largest element:", max);
